ad3f7299fdd115d14f0fd159e87c4f984fc46bc1
Sasha Fame <sadsparrow@mail.ru>
2022-11-10 21:14:46 +0600

    refactor: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,23 @@
 const fs = require('fs');
-const path = require('path')
-const dirPath = path.join(__dirname, 'files')
-
-console.log(dirPath)
+const path = require('path');
+const dirPath = path.join(__dirname, 'files');
 
 // Create folder
-fs.mkdir(
-    path.join(__dirname, 'files-copy'),{ recursive: true }, () => {
-        (err) => {
-            if(err) throw err;
-            console.log('Folder create')
-        }
-    }
-)
-
+fs.rm(path.resolve(__dirname, 'files-copy'), { recursive: true, force: true }, () => {
 fs.readdir(
     dirPath, { withFileTypes: true }, (err, files) => {
+
+
+        fs.mkdir(
+            path.join(__dirname, 'files-copy'),{ recursive: true }, () => {
+                (err) => {
+                    if(err) throw err;
+                    console.log('Folder create')
+                }
+            }
+        )
         files.forEach((item) => {
+
                 fs.readFile(
                     path.join(`${__dirname}/files`, item.name),
                     'utf-8',
@@ -34,4 +35,4 @@ fs.readdir(
                 );
         })
     }
-)
+)})
